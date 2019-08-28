a732111600d9c6d6569c94caa90697cefa2ca0ae
Andrew-Joseph Blanco <aj.blanco@rocketmail.com>
2019-08-27 20:26:32 -0500

    Done.

--- artist.rb
+++ artist.rb
@@ -1,2 +1,19 @@
 class Artist < ActiveRecord::Base
+
+    a = Artist.new(name: 'Jon')
+    # => #<Artist id: nil, name: "Jon", genre: nil, age: nil, hometown: nil>
+
+    a.age = 30
+    # => 30
+
+    a.save
+    # => true
+
+    # OR 
+
+    Artist.create(name: 'Kelly')
+    # => #<Artist id: 2, name: "Kelly", genre: nil, age: nil, hometown: nil>
+
+    # ------>Artist.find_by(name: 'Jon')
+    # => #<Artist id: 1, name: "Jon", genre: nil, age: 30, hometown: nil>
 end
